097ecd63e23c01e224ddb8101ddfedc6ce2fd7af
Yuriy Krasnov <118444989+Yuriy8585@users.noreply.github.com>
2022-12-06 19:47:32 +0300

    task19

---
+++ Task19/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая на вход принимает пятизначное число и проверяет, является ли оно палиндромом
+// 14212 = да
+// 23432 = нет
+// 12821 = да
+
+Console.Clear();
+Console.WriteLine("Enter number = ");
+int x = 0;
+int y;
+int z;
+
+int n = int.Parse(Console.ReadLine());
+z = n;
+
+while (n!=0)
+{
+    y = n%10;
+    x = x*10 + remy;
+    n = n/10;
+}
+
+if (z == x) 
+{ 
+    Console.WriteLine("PALL");
+}
+else 
+{
+    Console.WriteLine("NOT PALL");
+}
