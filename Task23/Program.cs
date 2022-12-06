820320816296c2f0c48949419b9146d355f7d0f8
Yuriy Krasnov <118444989+Yuriy8585@users.noreply.github.com>
2022-12-06 20:34:48 +0300

    mistakes

---
+++ Task23/Program.cs
@@ -0,0 +1,12 @@
+﻿// напишите программу которая принимает на вход N и выдает таблицу кубов от 1 до N
+
+Console.Clear();
+Console.Write("Enter number: ");
+int num = int.Parse(Console.ReadLine()!);
+
+string x = "";
+for (int i = 1; i <= num; i++)
+{
+x+= Math.Pow(i, 3).ToString()+ " ";
+}
+Console.WriteLine(x);
